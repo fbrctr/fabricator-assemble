b9d9adf8456eb5fed1401ec138c932a283659fe0
Luke Askew <lukeaskew@gmail.com>
2015-04-02 12:07:16 -0400

    resolve helper dir correctly

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ var registerHelpers = function () {
 
 	// get helper files
 	var resolveHelper = path.join.bind(null, __dirname, 'helpers');
-	var localHelpers = fs.readdirSync('helpers');
+	var localHelpers = fs.readdirSync(resolveHelper());
 	var userHelpers = options.helpers;
 
 	// register local helpers
