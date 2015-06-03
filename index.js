c0a3265a7cce195ce206853d98910d64c637be33
Yann Gouffon <github@yago-art.ch>
2015-06-03 15:15:48 +0200

    fix indent fail

--- index.js
+++ index.js
@@ -242,9 +242,9 @@ var buildContext = function (data) {
  * @return {String}
  */
 var toTitleCase = function(str) {
-		return str.replace(/(\-|_)/g, ' ').replace(/\w\S*/g, function(word) {
-				return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-		});
+	return str.replace(/(\-|_)/g, ' ').replace(/\w\S*/g, function(word) {
+		return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+	});
 };
 
 
