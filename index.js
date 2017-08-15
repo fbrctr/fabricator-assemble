c44ce9ceb56769f20811d5ef0d32ad58b778bb60
Nathanael Smith <nathanaelnsmith@gmail.com>
2017-08-14 20:59:02 -0700

    Added build data option

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ var defaults = {
 	data: ['src/data/**/*.{json,yml}'],
 
 	/**
+	 * Data to be merged into context
+	 * @type {(Object)}
+	 */
+	buildData: {},
+
+	/**
 	 * Markdown files containing toolkit-wide documentation
 	 * @type {(String|Array)}
 	 */
@@ -245,7 +251,7 @@ var buildContext = function (data, hash) {
 	var docs = {};
 	docs[options.keys.docs] = assembly.docs;
 
-	return _.assign({}, data, assembly.data, assembly.materialData, materials, views, docs, hash);
+	return _.assign({}, data, assembly.data, assembly.materialData, options.buildData, materials, views, docs, hash);
 
 };
 
